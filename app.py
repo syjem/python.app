4e8036a6e40c2235db8a78d632a206e3e4210b2a
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-04-14 21:00:57 +0800

    Flight functions, intro to OOP in python

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def square(n):
 for i in range(20):
     print(f"The square of {i} is {square(i)}.")
 
-# classes
+# OOP (classes)
 class Point():
     def __init__(self, input1, input2):
         self.x = input1
@@ -66,3 +66,30 @@ class Point():
 p = Point(2, 8)
 print(p.x)
 print(p.y)
+
+
+# Add passengers to flight(OOP)
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(5)
+
+people = ["Jem", "Kurt", "Karl", "Michael", "Mark", "Exequel"]
+
+for person in people:
+    if flight.add_passenger(person):
+        print(f"Added {person} to flight successfully.")
+    else:
+        print(f"No available seats for {person}.")
+
