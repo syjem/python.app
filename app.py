4de6d1d7398e8d42e0bfdc493a40d9d1c602b221
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-04-14 10:51:21 +0800

    practice more basic python codes

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
 
-print("Hello, World!")
+# positive or negative 
+n = int(input("Number: "))
+
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
+
+# sequences
+names = ["Harry", "Ron", "Ginny"]
+print(names[0])
+
+# Tuple
+coordinateX = 10.0
+coordinateY = 20.0
+coordinates = (10.0, 20.0)
+
+print(coordinates)
+
+
+
