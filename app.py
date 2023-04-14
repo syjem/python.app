92ec5bf62dcc634419ca011085ed068ab5648208
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-04-14 11:07:40 +0800

    more problema sets

--- app.py
+++ app.py
@@ -10,15 +10,32 @@ else:
     print("n is zero")
 
 # sequences
-names = ["Harry", "Ron", "Ginny"]
-print(names[0])
+name = ["Harry", "Ron", "Ginny"]
+print(name[0])
 
 # Tuple
 coordinateX = 10.0
 coordinateY = 20.0
 coordinates = (10.0, 20.0)
-
 print(coordinates)
+
+# lists of names
+names = ["Harry", "Ron", "Ginny", "Fred"]
+names.append("Draco")
+names.sort()
+print(names)
+
+# sets
+s = set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(5)
+s.add(5)
+s.remove(3)
+print(s)
+print(f"The set has {len(s)} elements.")
 
 
 
