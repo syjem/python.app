e85241e1ba83f74c9d6a31bca6fb90d691f83def
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-04-14 11:47:28 +0800

    class Point

--- app.py
+++ app.py
@@ -56,3 +56,13 @@ def square(n):
 
 for i in range(20):
     print(f"The square of {i} is {square(i)}.")
+
+# classes
+class Point():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+
+p = Point(2, 8)
+print(p.x)
+print(p.y)
