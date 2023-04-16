caf1865a03ee9df8e1cba2d3a5d7bb96f5cc75f3
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-04-16 19:30:24 +0800

    Computer guess game

---
+++ computer_guess.py
@@ -0,0 +1,22 @@
+import random
+
+# Let the computer guess your number
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)?? ").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print("Yay, congrats you guessed it!")
+
+computer_guess(1000)
