a1a6f7cedd11b23933a986bc03699370b7018e62
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-04-16 18:10:07 +0800

    mad_libs

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,36 @@
 
-name = input("Name: ")
-print(f"Hello, {name}!")
+# Learn string concatenations with mad_libs
+
+adjective = input("Adjective: ")
+adjective1 = input("Adjective: ")
+adjective2 = input("Adjective: ")
+adjective3 = input("Adjective: ")
+adjective4 = input("Adjective: ")
+adjective5 = input("Adjective: ")
+
+noun = input("Noun : ")
+noun1 = input("Noun : ")
+noun2 = input("Noun : ")
+noun3 = input("Noun : ")
+noun4 = input("Noun : ")
+noun5 = input("Noun : ")
+noun6 = input("Noun : ")
+noun7 = input("Noun : ")
+noun8 = input("Noun : ")
+noun9 = input("Noun : ")
+noun10 = input("Noun : ")
+
+verb = input("Verb: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+
+mad_lib = f"Once upon a time, in a {adjective} kingdom far, far away, there lived a {noun} who was known for their \
+{adjective1} {noun1} skills. One day, the {noun2} received a mysterious {noun3} from a {adjective2} {noun4}. The {noun5} \
+eagerly opened the {noun6} and found a {adjective3} {noun7} inside. It was a {adjective4} {noun8} that granted the possessor \
+the power to {verb} anything they desired. Excitedly, the {noun9} set off on a grand {adjective5} adventure, using their \
+newfound power to {verb1} their way through challenges and obstacles, and ultimately {verb2} the kingdom from an evil {noun10} \
+who was threatening to {verb3} everything."
+
+print(mad_lib)
+
